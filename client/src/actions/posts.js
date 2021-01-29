8857840c7ac5dc214baf03bf409f635d03b407a4
ysonbrian <znlsakxls@gmail.com>
2021-01-29 23:17:43 +0900

    Memories App Starts

---
+++ client/src/actions/posts.js
@@ -0,0 +1,20 @@
+import * as api from '../api';
+
+// Action Creators
+export const getPosts = () => async (dispatch) => {
+	try {
+		const { data } = await api.fetchPosts();
+		dispatch({ type: 'FETCH_ALL', payload: data });
+	} catch (error) {
+		console.log(error.meesage);
+	}
+};
+
+export const createPost = (post) => async (dispatch) => {
+	try {
+		const { data } = await api.createPost(post);
+		dispatch({ type: 'CREATE', payload: data });
+	} catch (error) {
+		console.log(error);
+	}
+};
